6f9703f3dab1c8a707d96c8345d6847de80d08c5
Kurt Guardia <kurtguardia@gmail.com>
2021-01-24 11:14:12 +0100

    Added script

---
+++ app.js
@@ -0,0 +1,23 @@
+const time = document.querySelector('.time');
+const today = new Date();
+const oneDay = 1000 * 60 * 60 * 24;
+const arrival = new Date(2021, 6, 1);
+const result = Math.round(arrival.getTime() - today.getTime()) / oneDay;
+const finalResult = result.toFixed(0);
+const arrivalMonths = Math.floor(finalResult / 30);
+const arrivalTotalDays = finalResult - arrivalMonths * 30;
+const arrivalExactWeeks = arrivalTotalDays / 7;
+const arrivalWeeks = arrivalExactWeeks.toFixed(0);
+const arrivalDays =
+  arrivalTotalDays - arrivalWeeks * 7 < 0
+    ? 0
+    : arrivalTotalDays - arrivalWeeks * 7;
+
+console.log(
+  `Arrival Months: ${arrivalMonths}, Arrival Total Days: ${arrivalTotalDays}, Arrival Weeks: ${arrivalWeeks}, Arrival Days: ${arrivalDays}`
+);
+
+time.innerHTML = `${arrivalMonths} meses, ${arrivalWeeks} semanas, ${arrivalDays} dias.`;
+
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+confetti();
